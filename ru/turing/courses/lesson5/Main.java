6d6c059bbec6c4c0ba61b7fe87692a5b56f4f3a0
diagorn <mikhail.gasin@fors.ru>
2023-03-28 16:37:31 +0300

    added lesson 5 task and code samples

---
+++ ru/turing/courses/lesson5/Main.java
@@ -0,0 +1,7 @@
+package ru.turing.courses.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        //todo вставить сюда сортировки пузырьком, сделать для каждой свой метод
+    }
+}
