7f910b1e46064b1041a49cf52c81df2ce6faeb68
diagorn <mikhail.gasin@fors.ru>
2023-04-02 17:13:55 +0300

    fix lesson 1 task

--- ru/turing/courses/lesson1/Solution.java
+++ ru/turing/courses/lesson1/Solution.java
@@ -2,6 +2,6 @@ package ru.turing.courses.lesson1;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //todo написать код здесь
     }
 }
