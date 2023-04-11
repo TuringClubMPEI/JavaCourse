903ec3b0735b60925ff5185855e8ed06520092f7
Nikita <nik.erofeeev@mail.ru>
2023-04-11 19:21:03 +0300

    дз урок 1

--- ru/turing/courses/lesson1/Erofeev/Main.java
+++ ru/turing/courses/lesson1/Erofeev/Main.java
@@ -6,6 +6,9 @@
 
  *******************************************************************************/
 import java.time.Period;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Scanner;
 import java.time.LocalDate;
@@ -17,40 +20,66 @@ public class Main
         Scanner in = new Scanner(System.in);
         //Ввод ФИО
         System.out.println("Пожалуйста, введите своё ФИО");
+
         System.out.println("Фамилия: ");
         String secondName = in.nextLine();
+
         System.out.println("Имя: ");
         String name = in.nextLine();
+
         System.out.println("Отчество: ");
         String surname = in.nextLine();
+
         //Ввод Даты Рождения
         System.out.println("Введите свою дату рождения в формате дд.мм.гггг: ");
-        String date = in.nextLine();
-        //разбиваем дату по "." в массив строк
-        String[] dateWords = date.split("\\.");
-        //Составляем новую строку-дату для преобразования в LocalDate объект
-        date = dateWords[2] + "-" + dateWords[1] + "-" + dateWords[1];
-        LocalDate newDate = LocalDate.parse(date);
+        String birthDateString = in.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthDateLocalDate = LocalDate.parse(birthDateString, formatter);
+
         //ввод адреса
         System.out.println("Введите свой адрес одной строкой (в формате 'страна: Россия, город: Москва, улица: Авиамоторная, дом: 15, квартира: 24)'");
         String address = in.nextLine();
+
         //Разбиваем адрес по ","
         String[] addressWords = address.split(", ");
+
         //Получаем кол-во полных лет между текущей датой и датой рождения
-        int age = Period.between(newDate, LocalDate.now()).getYears();
+        var period = Period.between(birthDateLocalDate, LocalDate.now());
+        int age = period.getYears();
+
         //Получаем кол-во полных месяцев между текущей датой и датой рождения
-        int month = Period.between(newDate, LocalDate.now()).getMonths();
+        int month = Period.between(birthDateLocalDate, LocalDate.now()).getMonths();
+
         //Вывод результата
         System.out.println("Количество полных лет: ");
         System.out.println(age);
+
         System.out.println("Количество полных месяцев: ");
-        System.out.println(month);
+        System.out.println( ChronoUnit.MONTHS.between(
+                YearMonth.from(birthDateLocalDate),
+                YearMonth.from(LocalDate.now())));
+
         System.out.println("Краткий формат адреса:");
         //В массиве строк с разбиением адреса, каждый элемент разбиваем по разделителю ": " и выводим второй элемент
         for (String word : addressWords) {
-            System.out.println(word.split(": ")[1]);
+            String[] separetedData = word.split(": ");
+
+            if(separetedData[0].equals ("страна")){
+                System.out.println(separetedData[1]);
+            }
+            if(separetedData[0].equals ("город")){
+                System.out.println("г." + separetedData[1]);
+            }
+            if(separetedData[0].equals ("улица")){
+                System.out.println("ул." + separetedData[1]);
+            }
+            if(separetedData[0].equals ("дом")){
+                System.out.println("д." + separetedData[1]);
+            }
+            if(separetedData[0].equals ("квартира")){
+                System.out.println("кв." + separetedData[1]);
+            }
         }
-        //Закрываем поток
-        in.close();
     }
 }
