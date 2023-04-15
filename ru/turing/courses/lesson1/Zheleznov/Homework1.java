07b600ef820c84d37ecebdf7420b1ab616c9ddc1
artemzelenov <artemzeleznov40@gmail.com>
2023-04-15 15:36:46 +0300

    corrected homework1

--- ru/turing/courses/lesson1/Zheleznov/Homework1.java
+++ ru/turing/courses/lesson1/Zheleznov/Homework1.java
@@ -1,9 +1,11 @@
 package ru.turing.courses.lesson1.Zheleznov;
+
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.time.LocalDate;
-public class homework1 {
+
+public class Homework1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,13 +13,13 @@ public class homework1 {
         System.out.println("Введите свое ФИО");
         String initiatives = scanner.nextLine();
         System.out.println("Введите дату рождения в формате: дд.мм.гггг");
-        String dateofbirth = scanner.nextLine();
+        String dateOfBirth = scanner.nextLine();
         System.out.println(" Введите свой адрес в формате: страна:, город:, улица:, дом:, квартира:");
         String address = scanner.nextLine();
 
 //считаю количество полных лет и месяцев
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate birthday = LocalDate.parse(dateofbirth, formatter);
+        LocalDate birthday = LocalDate.parse(dateOfBirth, formatter);
         LocalDate currentDate = LocalDate.now();
         Period livedlife = Period.between(birthday, currentDate);
         int years = livedlife.getYears();
@@ -25,13 +27,11 @@ public class homework1 {
         System.out.println("Полных лет: " + years + " полных месяцев: " + months);
 
 //делаю вывод в соответствии с заданием
-        String[] postfixes = new String[]{"", "г. ", "ул. ", "д. ", "кв. "};
+        String[] prefixes = new String[]{"", "г. ", "ул. ", "д. ", "кв. "};
         String[] addressmas = address.split(",");
-        int j = 0;
-        for (String i : addressmas) {
-            final String[] addressmassif = i.split(": ");
-            System.out.println(postfixes[j] + addressmassif[1]);
-            j++;
+        for (int i = 0; i < addressmas.length; i++) {
+            final String[] massif = addressmas[i].split(": ");
+            System.out.println(prefixes[i] + massif[1]);
         }
     }
 
