c93305385111c2681c01ce4cbc2a7d7e21b9c179
Валерия <booyashik@gmail.com>
2023-04-20 19:32:41 +0300

    класс mood

--- ru/turing/courses/lesson2/Shishlova/Study/StudingMood.java
+++ ru/turing/courses/lesson2/Shishlova/Study/StudingMood.java
@@ -1,2 +1,35 @@
-package ru.turing.courses.lesson2.Shishlova.Study;public class StudingMood {
+package ru.turing.courses.lesson2.Shishlova.Study;
+
+import ru.turing.courses.lesson2.Shishlova.Study.StudyingConsole;
+import ru.turing.courses.lesson2.Shishlova.Study.Studying;
+import ru.turing.courses.lesson2.Shishlova.Study.Study;
+
+class StudyingMood implements Studying {
+    private int mood; // настроение
+
+    public StudyingMood() {
+        mood = 50; // начальное настроение 50 из 100
+    }
+
+    // реализация метода study() для StudyingMood
+    public void study() {
+        mood -= 10; // учеба снижает настроение
+        System.out.println("Учу математику...");
+    }
+
+    // реализация метода passExam() для StudyingMood
+    public void passExam(boolean passed) {
+        if (passed) {
+            System.out.println("Экзамен сдан! :)");
+            mood += 20; // успешная сдача экзамена улучшает настроение
+        } else {
+            System.out.println("Экзамен не сдан :( Учись лучше!");
+            mood -= 20; // неуспешная сдача экзамена ухудшает настроение
+        }
+    }
+
+    // геттер для поля mood
+    public int getMood() {
+        return mood;
+    }
 }
