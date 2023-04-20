acb0bc00de5a8e70c53ff1b391f6951af5b02258
Валерия <booyashik@gmail.com>
2023-04-20 20:13:35 +0300

    новый импорт классов

--- ru/turing/courses/lesson2/Shishlova/Study/StudyingConsole.java
+++ ru/turing/courses/lesson2/Shishlova/Study/StudyingConsole.java
@@ -2,17 +2,19 @@ package ru.turing.courses.lesson2.Shishlova.Study;
 
 import ru.turing.courses.lesson2.Shishlova.Study.StudyingMood;
 import ru.turing.courses.lesson2.Shishlova.Study.Studying;
-import ru.turing.courses.lesson2.Shishlova.Study.Study;
+
 
 // вторая реализация интерфейса Studying
 public class StudyingConsole implements Studying {
 
     // реализация метода study() для StudyingConsole
+    @Override
     public void study() {
         System.out.println("Открываю консоль для учебы...");
     }
 
     // реализация метода passExam() для StudyingConsole
+    @Override
     public void passExam(boolean passed) {
         if (passed) {
             System.out.println("Экзамен сдан! :)");
