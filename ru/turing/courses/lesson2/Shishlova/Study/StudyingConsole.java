b725b89c56414016c4c643fccd7085c2b3b5f96a
Валерия <booyashik@gmail.com>
2023-04-20 19:33:04 +0300

    класс console

--- ru/turing/courses/lesson2/Shishlova/Study/StudyingConsole.java
+++ ru/turing/courses/lesson2/Shishlova/Study/StudyingConsole.java
@@ -1,2 +1,24 @@
-package ru.turing.courses.lesson2.Shishlova.Study;public class StudyingConsole {
+package ru.turing.courses.lesson2.Shishlova.Study;
+
+import ru.turing.courses.lesson2.Shishlova.Study.StudyingMood;
+import ru.turing.courses.lesson2.Shishlova.Study.Studying;
+import ru.turing.courses.lesson2.Shishlova.Study.Study;
+
+// вторая реализация интерфейса Studying
+public class StudyingConsole implements Studying {
+
+    // реализация метода study() для StudyingConsole
+    public void study() {
+        System.out.println("Открываю консоль для учебы...");
+    }
+
+    // реализация метода passExam() для StudyingConsole
+    public void passExam(boolean passed) {
+        if (passed) {
+            System.out.println("Экзамен сдан! :)");
+        } else {
+            System.out.println("Экзамен не сдан :( Учись лучше!");
+        }
+        System.out.println("Закрываю консоль.");
+    }
 }
