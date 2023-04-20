78ce4f175747884c42d925a6731b9781311e5b81
Валерия <booyashik@gmail.com>
2023-04-20 20:15:34 +0300

    убрала ненужное

--- ru/turing/courses/lesson2/Shishlova/Homework2.java
+++ ru/turing/courses/lesson2/Shishlova/Homework2.java
@@ -1,46 +1,17 @@
 package ru.turing.courses.lesson2.Shishlova;
 
 import ru.turing.courses.lesson2.Shishlova.Animal.Animal;
-import ru.turing.courses.lesson2.Shishlova.Study.Study;
 import ru.turing.courses.lesson2.Shishlova.Animal.Cat;
 import ru.turing.courses.lesson2.Shishlova.Animal.Dog;
 import ru.turing.courses.lesson2.Shishlova.Study.StudyingConsole;
+import ru.turing.courses.lesson2.Shishlova.UserProcessor;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.util.Objects;
 
 public class Homework2 {
-    public Homework2() {
-        super();
-    }
-
-    public static class UserProcessor {
-        // метод для подсчета полных лет пользователя
-        public static int calculateAgeInYears(LocalDate birthDate) {
-            return Period.between(birthDate, LocalDate.now()).getYears();
-        }
-
-        // метод для подсчета полных месяцев, которые пользователь прожил
-        public static long calculateAgeInMonths(LocalDate birthDate) {
-            return Period.between(birthDate, LocalDate.now()).toTotalMonths();
-        }
-
-        // метод для преобразования полного адреса пользователя в краткий формат
-        public static String getShortAddress(String fullAddress) {
-            String[] addressParts = fullAddress.split(", ");
-            String country = addressParts[0].split(": ")[1];
-            String city = addressParts[1].split(": ")[1];
-            String street = addressParts[2].split(": ")[1];
-            String house = addressParts[3].split(": ")[1];
-            String apartment = addressParts[4].split(": ")[1];
-            return country + "\n" + "г. " + city + "\n" + "ул. " + street + "\n" + "д. " + house + "\n" + "кв. " + apartment;
-        }
-    }
-
-
         public static void main (String[]args){
             // создаем объект типа Scanner для считывания пользовательского ввода
             Scanner scanner = new Scanner(System.in);
