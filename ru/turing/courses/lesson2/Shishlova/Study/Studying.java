ddc025246bbaa7ad7bc973c505333d41a845fe84
Валерия <booyashik@gmail.com>
2023-04-20 19:32:16 +0300

    класс studying

--- ru/turing/courses/lesson2/Shishlova/Study/Studying.java
+++ ru/turing/courses/lesson2/Shishlova/Study/Studying.java
@@ -1,2 +1,12 @@
-package ru.turing.courses.lesson2.Shishlova.Study;public interface Studying {
+package ru.turing.courses.lesson2.Shishlova.Study;
+
+import ru.turing.courses.lesson2.Shishlova.Study.StudyingConsole;
+import ru.turing.courses.lesson2.Shishlova.Study.StudyingMood;
+import ru.turing.courses.lesson2.Shishlova.Study.Study;
+
+// интерфейс Studying
+interface Studying {
+    void study();
+
+    void passExam(boolean passed);
 }
