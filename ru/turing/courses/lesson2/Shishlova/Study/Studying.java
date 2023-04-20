752642d2b0a802347d08c6965478861c68050c77
Валерия <booyashik@gmail.com>
2023-04-20 20:14:02 +0300

    новый импорт классов

--- ru/turing/courses/lesson2/Shishlova/Study/Studying.java
+++ ru/turing/courses/lesson2/Shishlova/Study/Studying.java
@@ -2,7 +2,6 @@ package ru.turing.courses.lesson2.Shishlova.Study;
 
 import ru.turing.courses.lesson2.Shishlova.Study.StudyingConsole;
 import ru.turing.courses.lesson2.Shishlova.Study.StudyingMood;
-import ru.turing.courses.lesson2.Shishlova.Study.Study;
 
 // интерфейс Studying
 interface Studying {
