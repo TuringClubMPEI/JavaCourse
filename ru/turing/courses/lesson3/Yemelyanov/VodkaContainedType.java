db45aa2b8db461c7f09b5d62e67b951f472efc0e
Steklotara_Blizz <zuxxlyes2015@gmail.com>
2023-04-25 18:21:36 +0300

    Revert "HW3 первая попытка (не работает removeByValue - в процессе)"
    This reverts commit e580eb0990369eb0083368d625dcd7618d10595a.

--- ru/turing/courses/lesson3/Yemelyanov/VodkaContainedType.java
+++
@@ -1,9 +0,0 @@
-package ru.turing.courses.lesson3.Yemelyanov;
-
-public class VodkaContainedType extends Alcohol{
-
-    public VodkaContainedType(String vodkaType, String itemName){
-        super(itemName);
-        this.itemId = vodkaType;
-    }
-}
