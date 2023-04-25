39a83b1b3cf3621e74094ddcafe874bfb6f2edda
Валерия <booyashik@gmail.com>
2023-04-25 22:51:52 +0300

    3 homework

---
+++ ru/turing/courses/lesson3/Shishlova/Homework3.java
@@ -0,0 +1,8 @@
+package ru.turing.courses.lesson3.Shishlova;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        // здесь можно вызывать методы класса Registry
+        Registry registry = new Registry();
+    }
+}
---
+++ ru/turing/courses/lesson3/Shishlova/Registry.java
@@ -0,0 +1,2 @@
+package ru.turing.courses.lesson3.Shishlova;public class Registry {
+}
