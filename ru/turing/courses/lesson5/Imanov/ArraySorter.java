4d353d9675d6684f6dfd710669b5d2089ec50dab
Imanov Timur <ImanovTB@mpei.ru>
2023-05-06 10:07:06 +0300

    minor change

--- ru/turing/courses/lesson5/Imanov/ArraySorter.java
+++ ru/turing/courses/lesson5/Imanov/ArraySorter.java
@@ -65,9 +65,7 @@ public class ArraySorter {
 
         for (int i = 0; i < array.length - 1; ++i){
             for (int j = 0; j < array.length - i - 1; ++j){
-                if (array[j] == null) {
-                    swap(array, j, j + 1);
-                } else if (array[j].compareTo(array[j + 1]) > 0){
+                if (array[j] == null || array[j].compareTo(array[j + 1]) > 0) {
                     swap(array, j, j + 1);
                 }
             }
