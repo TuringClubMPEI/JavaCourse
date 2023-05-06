af7637114ad82b3c2474026d4923855602a02171
Валерия <booyashik@gmail.com>
2023-05-06 03:53:34 +0300

    изменила по комментариям

--- ru/turing/courses/lesson3/Shishlova/Registry.java
+++ ru/turing/courses/lesson3/Shishlova/Registry.java
@@ -6,83 +6,44 @@ import java.util.List;
 import java.util.Map;
 
 public class Registry<T extends NamedObject> {
-    // HashMap для хранения записей
-    private Map<String, T> data = new HashMap<>();
+    private Map<String, T> objectsByKey = new HashMap<>();
 
-    /**
-     * Добавление новой записи в реестр
-     *
-     * @param key   ключ записи
-     * @param value значение записи
-     * @throws IllegalArgumentException если value == null
-     */
-    public void add(String key, T value) {
-        if (value == null) {
-            throw new IllegalArgumentException("Value cannot be null");
-        }
-        data.put(key, value);
+    public void add(String key, T object) {
+        objectsByKey.put(key, object);
     }
 
-    /**
-     * Удаление записи из реестра по заданному ключу
-     *
-     * @param key ключ записи
-     * @return значение удаленной записи или null, если такого ключа не было в реестре
-     */
-    public T removeByKey(String key) {
-        return data.remove(key);
-    }
-
-    /**
-     * Удаление всех записей из реестра по заданному значению
-     *
-     * @param value значение записи
-     * @return список ключей удаленных записей
-     */
-    public List<String> removeByValue(T value) {
-        List<String> deletedKeys = new ArrayList<>();
-        for (Map.Entry<String, T> entry : data.entrySet()) {
-            if (entry.getValue().equals(value)) {
-                deletedKeys.add(entry.getKey());
-            }
-        }
-        for (String key : deletedKeys) {
-            data.remove(key);
-        }
-        return deletedKeys;
-    }
-
-    /**
-     * Полная очистка реестра
-     */
-    public void clear() {
-        data.clear();
-    }
-
-    /**
-     * Поиск значения записи по ключу
-     *
-     * @param key ключ записи
-     * @return значение записи или null, если такого ключа не было в реестре
-     */
     public T getByKey(String key) {
-        return data.get(key);
+        return objectsByKey.get(key);
     }
 
-    /**
-     * Поиск значений записей по имени
-     *
-     * @param name начало имени записей для поиска
-     * @return список значений записей, у которых имя начинается с заданной подстроки
-     */
     public List<T> getByName(String name) {
-        List<T> values = new ArrayList<>();
-        for (T value : data.values()) {
-            if (value != null && value.getName() != null && value.getName().startsWith(name)) {
-                values.add(value);
+        List<T> objectsByName = new ArrayList<>();
+        for (T object : objectsByKey.values()) {
+            if (object.getName().toLowerCase().startsWith(name.toLowerCase())) {
+                objectsByName.add(object);
             }
         }
-        return values;
+        return objectsByName;
     }
 
+    public T removeByKey(String key) {
+        return objectsByKey.remove(key);
+    }
+
+    public List<String> removeByValue(T object) {
+        List<String> keysToRemove = new ArrayList<>();
+        for (Map.Entry<String, T> entry : objectsByKey.entrySet()) {
+            if (entry.getValue().equals(object)) {
+                keysToRemove.add(entry.getKey());
+            }
+        }
+        for (String key : keysToRemove) {
+            objectsByKey.remove(key);
+        }
+        return keysToRemove;
+    }
+
+    public void clear() {
+        objectsByKey.clear();
+    }
 }
