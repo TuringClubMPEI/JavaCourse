691dfd562127be37c2a8add44f03ae43f7ef4a92
dimafilippov <dimon.neet12@gmail.com>
2023-05-21 15:34:10 +0300

    fixed

--- ru/turing/courses/lesson4/filippov/NotFoundException.java
+++ ru/turing/courses/lesson4/filippov/NotFoundException.java
@@ -3,17 +3,11 @@ package ru.turing.courses.lesson4.filippov;
 public class NotFoundException extends RuntimeException {
     String name=null;
     public NotFoundException(String name) {
-        super();
+        super(name);
         this.name = name;
     }
     public NotFoundException() {
         super();
-    }@Override
-    public String getMessage() {
-        if (name==null || name.length() == 0) {
-            return "Вы ничего не ввели";
-        }
-        return "Такого enum нет";
     }
 }
 
