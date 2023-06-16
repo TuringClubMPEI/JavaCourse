274fb5e385cd25a6ab0a0d5655b81747d90ccc32
lefasdayan <ameracc22@gmail.com>
2023-06-16 14:33:05 +0300

    -поменял названия полей

--- ru/turing/courses/lesson6/privalov/ddl.sql
+++ ru/turing/courses/lesson6/privalov/ddl.sql
@@ -3,15 +3,15 @@
 -- object: public.debt | type: TABLE --
 -- DROP TABLE IF EXISTS public.debt CASCADE;
 create TABLE public.debt (
-	debt_id numeric(10) NOT NULL,
-	debt_name varchar(100),
+	id numeric(10) NOT NULL,
+	"name" varchar(100),
 	amount numeric(10,2),
 	currency_id numeric(10),
 	date_start date,
 	date_due date,
 	contact_id numeric(10),
-	debt_type_id numeric(10),
-	CONSTRAINT debt_pk PRIMARY KEY (debt_id)
+	type_id numeric(10),
+	CONSTRAINT debt_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.debt OWNER TO postgres;
@@ -20,10 +20,10 @@ ALTER TABLE public.debt OWNER TO postgres;
 -- object: public.currency | type: TABLE --
 -- DROP TABLE IF EXISTS public.currency CASCADE;
 create TABLE public.currency (
-	currency_id numeric(10) NOT NULL,
-	currency_name varchar(70),
+	id numeric(10) NOT NULL,
+	"name" varchar(70),
 	course_to_rubble numeric(10,7),
-	CONSTRAINT currency_pk PRIMARY KEY (currency_id)
+	CONSTRAINT currency_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.currency OWNER TO postgres;
@@ -32,12 +32,12 @@ ALTER TABLE public.currency OWNER TO postgres;
 -- object: public.account | type: TABLE --
 -- DROP TABLE IF EXISTS public.account CASCADE;
 create TABLE public.account (
-	acc_id numeric(10) NOT NULL,
-	acc_name varchar(50),
+	id numeric(10) NOT NULL,
+	"name" varchar(50),
 	amount numeric(10,2),
 	currency_id numeric(10),
 	bank_name varchar(50),
-	CONSTRAINT account_pk PRIMARY KEY (acc_id)
+	CONSTRAINT account_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.account OWNER TO postgres;
@@ -46,10 +46,10 @@ ALTER TABLE public.account OWNER TO postgres;
 -- object: public.debt_type | type: TABLE --
 -- DROP TABLE IF EXISTS public.debt_type CASCADE;
 create TABLE public.debt_type (
-	type_id numeric(10) NOT NULL,
-	type_name varchar(100),
-	type_note varchar(240),
-	CONSTRAINT debt_type_pk PRIMARY KEY (type_id)
+	id numeric(10) NOT NULL,
+	"name" varchar(100),
+	note varchar(240),
+	CONSTRAINT debt_type_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.debt_type OWNER TO postgres;
@@ -58,13 +58,13 @@ ALTER TABLE public.debt_type OWNER TO postgres;
 -- object: public.contact | type: TABLE --
 -- DROP TABLE IF EXISTS public.contact CASCADE;
 create TABLE public.contact (
-	contact_id numeric(10) NOT NULL,
-	contact_name varchar(70),
-	contact_surname varchar(70),
+	id numeric(10) NOT NULL,
+	"name" varchar(70),
+	surname varchar(70),
 	ph_number varchar(20),
 	bank_card_number numeric(16),
-	contact_notes varchar(500),
-	CONSTRAINT contact_pk PRIMARY KEY (contact_id)
+	note varchar(500),
+	CONSTRAINT contact_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 COMMENT ON COLUMN public.contact.ph_number IS E'Mobile phone number of contact';
@@ -77,15 +77,15 @@ ALTER TABLE public.contact OWNER TO postgres;
 -- object: public.transaction | type: TABLE --
 -- DROP TABLE IF EXISTS public.transaction CASCADE;
 create TABLE public.transaction (
-	transact_id numeric(10) NOT NULL,
-	transact_name varchar(50),
+	id numeric(10) NOT NULL,
+	"name" varchar(50),
 	amount numeric(10,2),
 	currency_id numeric(10),
 	date_time date,
-	transaction_type_id numeric(10),
+	type_id numeric(10),
 	from_acc_id numeric(10),
 	to_acc_id numeric(10),
-	CONSTRAINT transaction_pk PRIMARY KEY (transact_id)
+	CONSTRAINT transaction_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.transaction OWNER TO postgres;
@@ -98,20 +98,20 @@ create TABLE public.debt_history (
 );
 -- ddl-end --
 ALTER TABLE public.debt_history ADD CONSTRAINT debt_fk FOREIGN KEY (debt_id)
-REFERENCES public.debt (debt_id) MATCH FULL
+REFERENCES public.debt (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 ALTER TABLE public.debt_history ADD CONSTRAINT transact_fk FOREIGN KEY (transact_id)
-REFERENCES public.transaction (transact_id) MATCH FULL
+REFERENCES public.transaction (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 -- object: public.transaction_type | type: TABLE --
 -- DROP TABLE IF EXISTS public.transaction_type CASCADE;
 create TABLE public.transaction_type (
-	type_id numeric(10) NOT NULL,
-	type_name varchar(50),
-	type_description varchar(240),
-	CONSTRAINT transaction_type_pk PRIMARY KEY (type_id)
+	id numeric(10) NOT NULL,
+	"name" varchar(50),
+	note varchar(240),
+	CONSTRAINT transaction_type_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE public.transaction_type OWNER TO postgres;
@@ -119,57 +119,57 @@ ALTER TABLE public.transaction_type OWNER TO postgres;
 
 -- object: transaction_type_fk | type: CONSTRAINT --
 -- ALTER TABLE public.transaction DROP CONSTRAINT IF EXISTS transaction_type_fk CASCADE;
-ALTER TABLE public.transaction ADD CONSTRAINT transaction_type_fk FOREIGN KEY (transaction_type_id)
-REFERENCES public.transaction_type (type_id) MATCH FULL
+ALTER TABLE public.transaction ADD CONSTRAINT transaction_type_fk FOREIGN KEY (type_id)
+REFERENCES public.transaction_type (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: currency_fk | type: CONSTRAINT --
 -- ALTER TABLE public.debt DROP CONSTRAINT IF EXISTS currency_fk CASCADE;
 ALTER TABLE public.debt ADD CONSTRAINT currency_fk FOREIGN KEY (currency_id)
-REFERENCES public.currency (currency_id) MATCH FULL
+REFERENCES public.currency (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: currency_fk | type: CONSTRAINT --
 -- ALTER TABLE public.account DROP CONSTRAINT IF EXISTS currency_fk CASCADE;
 ALTER TABLE public.account ADD CONSTRAINT currency_fk FOREIGN KEY (currency_id)
-REFERENCES public.currency (currency_id) MATCH FULL
+REFERENCES public.currency (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: account_fk | type: CONSTRAINT --
 -- ALTER TABLE public.transaction DROP CONSTRAINT IF EXISTS account_fk CASCADE;
 ALTER TABLE public.transaction ADD CONSTRAINT account_fk FOREIGN KEY (from_acc_id)
-REFERENCES public.account (acc_id) MATCH FULL
+REFERENCES public.account (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: account_fk1 | type: CONSTRAINT --
 -- ALTER TABLE public.transaction DROP CONSTRAINT IF EXISTS account_fk1 CASCADE;
 ALTER TABLE public.transaction ADD CONSTRAINT account_fk1 FOREIGN KEY (to_acc_id)
-REFERENCES public.account (acc_id) MATCH FULL
+REFERENCES public.account (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: currency_fk | type: CONSTRAINT --
 -- ALTER TABLE public.transaction DROP CONSTRAINT IF EXISTS currency_fk CASCADE;
 ALTER TABLE public.transaction ADD CONSTRAINT currency_fk FOREIGN KEY (currency_id)
-REFERENCES public.currency (currency_id) MATCH FULL
+REFERENCES public.currency (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: contact_fk | type: CONSTRAINT --
 -- ALTER TABLE public.debt DROP CONSTRAINT IF EXISTS contact_fk CASCADE;
 ALTER TABLE public.debt ADD CONSTRAINT contact_fk FOREIGN KEY (contact_id)
-REFERENCES public.contact (contact_id) MATCH FULL
+REFERENCES public.contact (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: debt_type_fk | type: CONSTRAINT --
 -- ALTER TABLE public.debt DROP CONSTRAINT IF EXISTS debt_type_fk CASCADE;
-ALTER TABLE public.debt ADD CONSTRAINT debt_type_fk FOREIGN KEY (debt_type_id)
-REFERENCES public.debt_type (type_id) MATCH FULL
+ALTER TABLE public.debt ADD CONSTRAINT debt_type_fk FOREIGN KEY (type_id)
+REFERENCES public.debt_type (id) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
--- ru/turing/courses/lesson6/privalov/dml.sql
+++ ru/turing/courses/lesson6/privalov/dml.sql
@@ -1,22 +1,22 @@
 INSERT INTO public.currency(
-	currency_id, currency_name, course_to_rubble)
+	id, "name", course_to_rubble)
 	VALUES (1, 'rouble', 1),
 	(2, 'dollar', 70.25);
 
 INSERT INTO public.contact(
-	contact_id, contact_name, contact_surname, ph_number, bank_card_number, contact_notes)
+	id, "name", surname, ph_number, bank_card_number, note)
 	VALUES 
 	(1, 'Левый', 'Чел', '+79096969069', 2200220022002200, null),
 	(2, 'Правый', 'Челик', '+71414141488', 2202220222022202, null);
 
 INSERT INTO public.debt_type(
-	type_id, type_name, type_note)
+	id, "name", note)
 	VALUES 
 	(1, 'исходящий', 'пользователю должны денег'),
 	(2, 'входящий', 'пользователь должен денег');
 
 INSERT INTO public.transaction_type(
-	type_id, type_name, type_description)
+	id, "name", note)
 	VALUES 
 	(1, 'Пополнение', null),
 	(2, 'Покупка', null),
@@ -26,7 +26,7 @@ INSERT INTO public.transaction_type(
     (6, 'Перевод между счетами', null);
 
 INSERT INTO public.account(
-	acc_id, acc_name, amount, currency_id, bank_name)
+	id, "name", amount, currency_id, bank_name)
 	VALUES 
 	(1, 'Мэишная карта', 12500, 1, 'Сбер'),
 	(2, 'Основная карта', 1520.73, 1, 'Тинькофф'),
@@ -34,7 +34,7 @@ INSERT INTO public.account(
 	(4, 'Долларовый счет', 2.36, 2, 'Тинькофф');
 
 INSERT INTO public.transaction(
-	transact_id, transact_name, amount, currency_id, date_time, transaction_type_id, from_acc_id, to_acc_id)
+	id, "name", amount, currency_id, date_time, type_id, from_acc_id, to_acc_id)
 	VALUES 
 	(1, 'Пятерочка', 540.23, 1, '10.06.2023', 2, 1, null),
 	(2, 'Табачка', 179, 1, '11.06.2023', 2, 2, null),
@@ -44,7 +44,7 @@ INSERT INTO public.transaction(
 	(6, 'Перевод долга', 3000, 1, '20.06.2023', 4, null, 1);
 
 INSERT INTO public.debt(
-	debt_id, debt_name, amount, currency_id, date_start, date_due, contact_id, debt_type_id)
+	id, "name", amount, currency_id, date_start, date_due, contact_id, type_id)
 	VALUES 
 	(1, 'Долг за пиццерию', 4000, 1, null, '1.7.2023', 1, 2),
 	(2, 'За еду', 269.60, 1, '10.06.2023', '15.06.2023', 1, 1);
@@ -58,36 +58,36 @@ INSERT INTO public.debt_history(
 
 
 
-select cont.contact_name||' '||cont.contact_surname as FI, 
-	sum(debt.amount), dt.type_name
+select cont."name"||' '||cont.surname as FI, 
+	sum(debt.amount), dt."name"
 from public.contact as cont, public.debt as debt, public.debt_type as dt
-where cont.contact_id=debt.contact_id and dt.type_id=debt.debt_type_id
-group by FI, dt.type_name;
+where cont.id=debt.contact_id and dt.id=debt.type_id
+group by FI, dt."name";
 
-select tt.type_name, sum(tr.amount)
+select tt."name", sum(tr.amount)
 from public.transaction as tr, public.transaction_type as tt
-where tr.transaction_type_id=tt.type_id
+where tr.type_id=tt.id
 group by 1
 order by 2 desc;
 
-select acc.acc_name, count(tr.transact_id)
+select acc."name", count(tr.id)
 from public.account as acc, public.transaction as tr
-where acc.acc_id=tr.from_acc_id or acc.acc_id=tr.to_acc_id
+where acc.id=tr.from_acc_id or acc.id=tr.to_acc_id
 group by 1
 order by 2 desc;
 
-select dt.type_name||' долг', sum(d.amount)
+select dt."name"||' долг', sum(d.amount)
 from public.debt_type as dt, public.debt as d
-where dt.type_id=d.type_id_debt_type
+where dt.id=d.type_id
 group by 1
 order by 2 desc;
 
-select tt.type_name, count(tr.transact_id)
+select tt."name", count(tr.id)
 from public.transaction as tr, public.transaction_type as tt
-where tr.transaction_type_id=tt.type_id and tr.transaction_type_id=2
+where tr.type_id=tt.id and tr.type_id=2
 group by 1;
 
-select tr.transact_name, tr.amount, tr.date_time, debt.debt_name
+select tr."name", tr.amount, tr.date_time, debt."name"
 from public.transaction as tr, public.debt as debt, public.debt_history as dh
-where tr.transact_id=dh.transact_id and dh.debt_id=debt.debt_id and debt.debt_id=1
+where tr.id=dh.transact_id and dh.debt_id=debt.id and debt.id=1
 order by 3 desc;
