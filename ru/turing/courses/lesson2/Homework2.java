e39bc4150420e7b093479b2e2bcab3997c5d2eaf
damatvey <damatveywork@gmail.com>
2023-11-15 22:49:10 +0300

    Выполнил hw2

---
+++ ru/turing/courses/lesson2/Homework2.java
@@ -0,0 +1,79 @@
+package ru.turing.courses.lesson2;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        //подключаю библиотеку и создаю переменную с текущим годом
+        Calendar calendar = new GregorianCalendar();
+        int currentYear = calendar.get(Calendar.YEAR);
+
+        //ввод фио пользователя
+        System.out.println("Введите свое ФИО: ");
+        Scanner scan = new Scanner(System.in);
+        String fio = scan.nextLine();
+
+        //ввод даты
+        System.out.println("Введите свою дату рождеиня в формате дд.мм.гггг: ");
+        String date = scan.nextLine();
+        //создаю массив типа чар и начиная с 6 символа массива беру 4 символа года, преобразую в тип int и записываю в переменную year
+        char[] dateArray = date.toCharArray();
+        int year = Integer.valueOf(String.valueOf(dateArray, 6, 4));
+
+        //ввод пользователем адресса
+        System.out.println("Введите свой адресс одной строкой (Пример - страна: Россия, город: Москва, улица: Авиамоторная, дом: 15, квартира: 24)");
+        //создаю символ который в дальнейшем буду удалаять из нужной мне перепенной
+        char charToRemove = ',';
+
+        //для каждой переменной создаю переменну-буфер, в которой хранится страна:, город: и т.п.
+        String stranaBuffer = scan.next();
+        String strana = scan.next();
+        //удаляю запятую. Без этой строчки вывод переменной strana выглядел бы так -> Россия,
+        strana = strana.replace(String.valueOf(charToRemove), "");
+
+        //Повторяю те же действия для каждой составляющей адресса
+        String gorodBuffer = scan.next();
+        String gorod = scan.next();
+        gorod = gorod.replace(String.valueOf(charToRemove), "");
+
+        String ulicaBuffer = scan.next();
+        String ulica = scan.next();
+        ulica = ulica.replace(String.valueOf(charToRemove), "");
+
+        String domBuffer = scan.next();
+        String dom = scan.next();
+        dom = dom.replace(String.valueOf(charToRemove), "");
+
+        String kvartiraBuffer = scan.next();
+        String kvartira = scan.next();
+        kvartira = kvartira.replace(String.valueOf(charToRemove), "");
+
+        //считаю количество полных лет и месяцев пользователя
+
+        System.out.println("Количество полных лет ползователя: " + countAge(currentYear, year));
+
+
+        System.out.println("Количество полных месяцев ползователя: " + countMonth(countAge(currentYear, year)));
+
+        //перевожу адрес пользователя в краткий формат каждый пункт с новой строки
+        System.out.println(strana);
+        System.out.println("г." + gorod);
+        System.out.println("ул." + ulica);
+        System.out.println("д." + dom);
+        System.out.println("кв." + kvartira);
+
+    }
+
+    public static int countAge(int currentYear, int year) {
+        int ageOfUser = currentYear - year;
+        return ageOfUser;
+    }
+
+    public static int countMonth(int ageOfUser){
+        int monthOfUser = ageOfUser * 12;
+        return monthOfUser;
+    }
+}
--- ru/turing/courses/lesson2/animals/Animal.java
+++ ru/turing/courses/lesson2/animals/Animal.java
@@ -1,4 +1,73 @@
 package ru.turing.courses.lesson2.animals;
 
 public class Animal {
+    protected String klichka;
+    protected String color;
+    protected String breed;
+    protected int age;
+
+    public String getKlichka() {
+        return klichka;
+    }
+
+    public void setKlichka(String klichka) {
+        this.klichka = klichka;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    protected int numberOfPaws;
+
+    public void play(String anyActivity){
+
+    }
+
+    public void sleep(int hoursOfSleep){
+
+    }
+
+    public void eat(String eat){
+
+    }
+
+    //напишу конструктор к классу в нутри класса
+
+    public Animal(String klichka, String color, String breed, int age, int numberOfPaws) {
+        this.klichka = klichka;
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+        this.numberOfPaws = numberOfPaws;
+    }
 }
+
