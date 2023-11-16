d3656932652f955d9dc783ee1ae2e062981b2f8a
Orange-tt <115097828+Orange-tt@users.noreply.github.com>
2023-11-16 22:20:43 +0300

    Update Animal.java

--- ru/turing/courses/lesson2/animals/Animal.java
+++ ru/turing/courses/lesson2/animals/Animal.java
@@ -1,4 +1,31 @@
 package ru.turing.courses.lesson2.animals;
 
-public class Animal {
+public abstract class Animal {
+    // статические поля
+    protected static int hungerRate;
+
+    // динамические поля
+    protected int age;
+    protected int hunger;
+    protected String name;
+
+    public void Animal(int age, int hunger, String name) {
+        if(name ==null)
+            return;
+        this.age = age;
+        this.hunger = hunger;
+        this.name = name;
+    }
+
+    public void Eat(int nutrition) {
+        hunger -= nutrition;
+    }
+
+    protected void GetHungry(int time) {
+        hunger += time * hungerRate;
+    }
+
+    public abstract void Age(int time);
+  
 }
+
