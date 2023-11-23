da522a8e5d22a21523e163a922f8155938244c0e
Daniil Silver <148889410+DaniilSilver@users.noreply.github.com>
2023-11-24 00:55:46 +0300

    Первая домашняя работа(Попытка №2) Ну теперь уж точно =)))

--- ru/turing/courses/lesson2/animals/Bird.java
+++ ru/turing/courses/lesson2/animals/Bird.java
@@ -1,18 +1,19 @@
 package ru.turing.courses.lesson2.animals;
 
-public class bird extends Animal {
+public class Bird extends Animal {
     private boolean migration;
     //true - letit na south,false-no
     private String singing;
 
-    public bird(String vid, boolean eatingmeat, int age, boolean wild, boolean migration, String singing) {
+    public Bird(String vid, boolean eatingmeat, int age, boolean wild, boolean migration, String singing) {
         super(vid, eatingmeat, age, wild);
         this.migration = migration;
         this.singing = singing;
     }
 
+    @Override
     public void makeSound() {
         System.out.println(this.singing);
     }
-    }
+}
 
