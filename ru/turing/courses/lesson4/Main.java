75f0a61a4bb6fdf8dde70efb7e95ac893af88cf5
noinvited <bekas2003@mail.ru>
2023-11-26 17:14:32 +0300

    added new homework №3

--- ru/turing/courses/lesson4/Main.java
+++ ru/turing/courses/lesson4/Main.java
@@ -2,22 +2,28 @@ package ru.turing.courses.lesson4;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            // позитивные сценарии
+            Gender maleGender = Gender.ofName("MALE");
+            Gender femaleGender = Gender.ofName("FEMALE");
 
-        //todo реализовать с учетом выбросов эксепшнов следующие кейсы
+            Gender maleLowerCaseCharacter = Gender.ofName("male");
+            Gender femaleLowerCaseCharacter = Gender.ofName("female");
 
-        // позитивные сценарии
-        Gender maleGender = Gender.ofName("MALE");
-        Gender femaleGender = Gender.ofName("FEMALE");
+            Gender maleGenderByChar = Gender.ofShortNameRepr('M');
+            Gender femaleGenderByChar = Gender.ofShortNameRepr('F');
 
-        Gender maleLowerCaseCharacter = Gender.ofName("male");
-        Gender femaleLowerCaseCharacter = Gender.ofName("female");
+            Gender maleGenderByCharCharacter = Gender.ofShortNameRepr('m');
+            Gender femaleGenderByCharCharacter = Gender.ofShortNameRepr('f');
 
-        Gender maleGenderByChar = Gender.ofShortNameRepr('M');
-        Gender femaleGenderByChar = Gender.ofShortNameRepr('F');
-
-        // негативные сценарии
-        Gender nullGender = Gender.ofName(null);
-        Gender falseNameGender = Gender.ofName("Attack helicopter");
-        Gender falseCharReprName = Gender.ofShortNameRepr('\n');
+            // негативные сценарии
+            Gender nullGender = Gender.ofName(null);
+            Gender falseNameGender = Gender.ofName("Attack helicopter");
+            Gender falseCharReprName = Gender.ofShortNameRepr('\n');
+        } catch (IllegalGenderException e) {
+            System.out.println("Попытка создания нового гендера: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
