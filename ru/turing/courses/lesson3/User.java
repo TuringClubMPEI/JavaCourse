ea18d38aab73c6c47352709546c60d7686b38109
OronStor <VisperH@gmail.com>
2023-12-03 20:08:29 +0300

    ctrl+alt+L

---
+++ ru/turing/courses/lesson3/User.java
@@ -0,0 +1,46 @@
+/**
+ * User class
+ * @autor Ivanov Andrew
+ * @version 1.0
+ */
+package ru.turing.courses.lesson3;
+
+import java.util.Objects;
+
+public class User extends PersonEntry{
+    /**
+     * Username field
+     */
+    private String name;
+    /**
+     * Constructor by name&key
+     */
+    public User(String key,String name) {
+        super(key);
+        this.name = name;
+    }
+    /**
+     * Overriding the abstract class method
+     */
+    @Override
+    public String getValue() {
+        return name;
+    }
+    /**
+     * Overriding .equals() for comparison
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+    /**
+     * Overriding .hashCode() to get hashcode by name
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
