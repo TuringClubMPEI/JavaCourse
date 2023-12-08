95bfd7c3e4881217314604ffdcdd9ebac00a536e
KobrinN <nikkobrin2002@mail.ru>
2023-12-08 18:00:14 +0300

    HW-5 -сделал класс Employee; -сделал 3 стат. метода в Main, для сортировки разных типов данных; -проверил работу методов сортировки;

---
+++ ru/turing/courses/lesson5/Employee.java
@@ -0,0 +1,67 @@
+package ru.turing.courses.lesson5;
+
+import java.util.Objects;
+
+/**
+ * Класс с тремя полями типа Long, String и String для сортировки.
+ */
+public class Employee {
+    private Long id;
+    private String name;
+    private String surname;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname);
+    }
+}
--- ru/turing/courses/lesson5/Main.java
+++ ru/turing/courses/lesson5/Main.java
@@ -1,7 +1,178 @@
 package ru.turing.courses.lesson5;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Main {
+    /**
+     * Сортировка пузырьком массива целых чисел от большей длины к меньшей.
+     *
+     * @param arr массив, который нужно отсортировать.
+     * @return отсортированный массив.
+     * @throws NullPointerException генерирует исключение, если массив ссылается на null.
+     */
+    public static int[] sortMaxToMin(int[] arr) throws NullPointerException {
+        if (arr == null) {
+            throw new NullPointerException();
+        } else {
+            for (int j = 1; j < arr.length; j++) {
+                boolean flg = false;
+                for (int i = 0; i < arr.length - j; i++) {
+                    if (arr[i] < arr[i + 1]) {
+                        int buff = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = buff;
+                        flg = true;
+                    }
+                }
+                if (!flg) break;
+            }
+            return arr;
+        }
+    }
+
+    /**
+     * Сортировка пузырьком массива строк от большей длины к меньшей.
+     *
+     * @param arr массив, который нужно отсортировать.
+     * @return отсортированный массив.
+     * @throws NullPointerException генерирует исключение, если массив ссылается на null.
+     */
+    public static String[] sortMaxToMin(String[] arr) throws NullPointerException {
+        if (arr == null) {
+            throw new NullPointerException();
+        } else {
+            for (int j = 1; j < arr.length; j++) {
+                boolean flg = false;
+                for (int i = 0; i < arr.length - j; i++) {
+                    if (arr[i] == null) {
+                        if (arr[i + 1] != null) {
+                            arr[i] = arr[i + 1];
+                            arr[i + 1] = null;
+                            flg = true;
+                        }
+                    } else {
+                        if ((arr[i + 1] != null) && ((arr[i].length() < arr[i + 1].length()))) {
+                            StringBuilder buff = new StringBuilder(arr[i + 1]);
+                            arr[i + 1] = arr[i];
+                            arr[i] = buff.toString();
+                            flg = true;
+                        }
+                    }
+                }
+                if (!flg) break;
+            }
+            return arr;
+        }
+    }
+
+    /**
+     * Сортировка пузырьком массива собственного класса от большего к меньшему по всем полям этого класса.
+     *
+     * @param arr массив, который нужно отсортировать.
+     * @return отсортированный массив.
+     * @throws NullPointerException генерирует исключение, если массив ссылается на null.
+     */
+    public static Employee[] sortMaxToMin(Employee[] arr) throws NullPointerException {
+        if (arr == null) {
+            throw new NullPointerException();
+        } else {
+            for (int j = 1; j < arr.length; j++) {
+                boolean flg = false;
+                for (int i = 0; i < arr.length - j; i++) {
+                    if (arr[i].getId() == null) {
+                        if (arr[i + 1].getId() != null) {
+                            Employee buff = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = buff;
+                            flg = true;
+                        }
+                    } else {
+                        if (arr[i + 1].getId() != null) {
+                            if (arr[i].getId() < arr[i + 1].getId()) {
+                                Employee buff = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = buff;
+                                flg = true;
+                            } else if (arr[i].getId().equals(arr[i + 1].getId())) {
+                                if (arr[i].getName() == null) {
+                                    if (arr[i + 1].getName() != null) {
+                                        Employee buff = arr[i + 1];
+                                        arr[i + 1] = arr[i];
+                                        arr[i] = buff;
+                                        flg = true;
+                                    }
+                                } else {
+                                    if (arr[i + 1].getName() != null) {
+                                        if (arr[i].getName().length() < arr[i + 1].getName().length()) {
+                                            Employee buff = arr[i + 1];
+                                            arr[i + 1] = arr[i];
+                                            arr[i] = buff;
+                                            flg = true;
+                                        } else if (arr[i].getName().length() == arr[i + 1].getName().length()) {
+                                            if (arr[i].getSurname() == null) {
+                                                if (arr[i + 1].getSurname() != null) {
+                                                    Employee buff = arr[i + 1];
+                                                    arr[i + 1] = arr[i];
+                                                    arr[i] = buff;
+                                                    flg = true;
+                                                }
+                                            } else {
+                                                if (arr[i + 1].getSurname() != null) {
+                                                    if (arr[i].getSurname().length() < arr[i + 1].getSurname().length()) {
+                                                        Employee buff = arr[i + 1];
+                                                        arr[i + 1] = arr[i];
+                                                        arr[i] = buff;
+                                                        flg = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (!flg) break;
+            }
+            return arr;
+        }
+    }
+
     public static void main(String[] args) {
-        //todo вставить сюда сортировки пузырьком, сделать для каждой свой метод
+        var arrEmployee = new Employee[5];
+        var arrIntId = new int[5];
+        var arrStrName = new String[5];
+        {
+            arrEmployee[0] = new Employee(1L, "oleg", "olegov");
+            arrEmployee[1] = new Employee(null, "oleg", "orlov");
+            arrEmployee[2] = new Employee(6L, null, "kuskovov");
+            arrEmployee[3] = new Employee(2L, "kolya", null);
+            arrEmployee[4] = new Employee(10L, "artem", "olegovov");
+        }
+        for (int i = 0; i < arrEmployee.length; i++) {
+            if (arrEmployee[i].getId() != null) {
+                arrIntId[i] = arrEmployee[i].getId().intValue();
+            } else {
+                arrIntId[i] = 0;
+            }
+            arrStrName[i] = arrEmployee[i].getName();
+        }
+        try {
+            var sortArrEmployee = Main.sortMaxToMin(arrEmployee);
+            var sortArrIntId = Main.sortMaxToMin(arrIntId);
+            var sortArrStrName = Main.sortMaxToMin(arrStrName);
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println(sortArrIntId[i]);
+                System.out.println(sortArrStrName[i]);
+                System.out.println(sortArrEmployee[i]);
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("Есть массив null!");
+        }
+
     }
 }
